44651dc5ada08d856f8eb257b775b3a4d11bf97e
nirmeshbansal <cerealkillercode@gmail.com>
2019-10-28 22:53:15 +0530

    Added tokenizer'

--- train.py
+++ train.py
@@ -1,7 +1,16 @@
 import tensorflow as tf
+import pandas as pd
+import numpy
 from tensorflow.keras.preprocessing.text import Tokenizer
 tokenizer = Tokenizer(num_words = 5, oov_token = '-1')
-sentence = ['hello boi you are weird','sup my niggsy piggsy as as as as fd fdf e ed sc gdf s e s as das fdaf ']
-tokenizer.fit_on_texts(sentence)
-word_index = tokenizer.word_index
-print(word_index)
+trainset = pd.read_csv('aossie-click-bait-dataset/clickBait_Data.csv')
+print(df.head())
+del trainset['index']
+del trainset['id']
+del trainset['clickbait']
+trainlabel = pd.read_csv('aossie-click-bait-dataset/clickBait_Data.csv')
+del trainlabel['index']
+del trainlabel['id']
+del trainlabel['titles']
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(trainset)
